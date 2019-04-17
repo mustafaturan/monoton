7a626944f5b99413c387588a6269b3bc5bb3af8c
Mustafa Turan <mustafaturan.net@gmail.com>
2019-04-17 00:12:16 -0700

    Pass the directly available time function instead of anonymous function for nanosecond sequencer

--- sequencer/nanosecond.go
+++ sequencer/nanosecond.go
@@ -11,7 +11,7 @@ import (
 // NewNanosecond returns the preconfigured nanosecond sequencer
 func NewNanosecond() *Sequence {
 	return &Sequence{
-		now:     func() uint { return mtimer.Now() },
+		now:     mtimer.Now,
 		max:     62*62 - 1,
 		maxTime: uint(1<<64 - 1),
 	}
